789889b3016d52a59c35ded1c9a665a6221aa443
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2024-04-25 14:59:43 +0200

    rename syslog / implement daemon / implement config

--- cos/packages/cos_syslog.lua
+++ cos/packages/cos_syslog.lua
@@ -1,8 +1,8 @@
 syslog = {}
 
 syslog.metadata = {
-    name = "syslog",
-    description = "A simple logging system",
+    name = "cos_syslog",
+    description = "A simple logging system for cOS",
     author = "EmmaKnijn",
     license = "MIT",
     dependencies = {"redrun"}
@@ -10,11 +10,11 @@ syslog.metadata = {
 
 syslog.startup = function(config)
     if not config and not config.path then
-        logPath = "syslog"
+        logPath = "syslog.txt"
     else
         logPath = config.path
     end
-    
+    _G.cos_packages_config["cos_syslog"].logPath = logPath
     -- This is probably a sin but whatever
     _G.log = function(message,printed,level)
         if printed then        
@@ -34,12 +34,14 @@ syslog.startup = function(config)
     if config.daemon then
         shell.setPath(shell.path() .. ":/cos/programs/syslog")
         _G.cos_packages.redrun.start(function() -- start the background daemon
-            local event, command, arg1, arg2 = os.pullEvent("syslog_daemon")
-            if command == "clearLog" then
-                local file = fs.open(logPath, "w")
-                file.close()
-            elseif not command then
-                os.queueEvent("syslog_daemon_response")
+            while true do
+              local event, command, arg1, arg2 = os.pullEvent("syslog_daemon")
+              if command == "clearLog" then
+                  local file = fs.open(logPath, "w")
+                  file.close()
+              elseif command == "ping" then
+                  os.queueEvent("syslog_daemon_response")
+              end
             end
         end, "syslog_daemon")
     end
