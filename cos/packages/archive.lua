1d85b30d3315c0b6674f1c1a22f3ee52a4486677
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2024-04-25 15:47:56 +0200

    add archive package

---
+++ cos/packages/archive.lua
@@ -0,0 +1,22 @@
+local archive = {}
+archive.metadata = {
+    name = "archive",
+    description = "A simple archival tool for ComputerCraft",
+    author = "JackMacWindows",
+    license = "CC0",
+    dependencies = {"libdeflate"}
+}
+archive.startup = function()
+    local hweb, err = http.get("https://raw.githubusercontent.com/MCJack123/CC-Archive/master/archive.lua")
+    if not hweb then
+        log("Failed to download archive.lua: " .. err, false, "error")
+        error("Failed to download archive.lua: " .. err)
+    end
+    local hfile = fs.open("/cos/lib/archive.lua", "w")
+    hfile.write(hweb.readAll())
+    hfile.close()
+    hweb.close()
+
+    return true
+end
+return archive
