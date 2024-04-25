1b31bf0724540eb29acfde16742fd4283985bb7e
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2024-04-25 16:45:10 +0200

    add cleanup functionality to the daemon to remove unused packages

--- cos/packages/cos_daemon.lua
+++ cos/packages/cos_daemon.lua
@@ -13,6 +13,20 @@ daemon.startup = function()
             local event, command, arg1, arg2 = os.pullEvent("cos_daemon")
             if command == "listActive" then 
                 os.queueEvent("cos_daemon_response",  _G.cos_loaded_packages)
+            elseif command == "cleanup" then
+                for packageName,o in pairs(_G.cos_downloaded_packages) do
+                    if not _G.cos_loaded_packages[packageName] then
+                        local package = require("cos/packages/" .. packageName)
+                        if package.cleanup then
+                            package.cleanup()
+                            _G.cos_downloaded_packages[package] = nil
+                        else
+                            log("Package " .. packageName .. " has no cleanup function and couldn't be cleaned up", false, "error")
+                        end
+                        
+                        
+                    end
+                end
             end
         end
     end, "cos_daemon")
