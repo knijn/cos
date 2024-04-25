fc85c7d7acfcd71722ff36e3a20f4ce84a408610
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2024-04-25 16:35:26 +0200

    add a default config to copy into the newly installed system

---
+++ cos/programs/installer/data/defaultconfig.lua
@@ -0,0 +1,10 @@
+return {
+    silent_startup = false,
+    packages = {
+        cos_syslog = {
+            path = "/.syslog",
+            daemon = true
+        },
+        cos_daemon = {}
+    }
+}
