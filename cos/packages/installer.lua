2a7727fed06f6f81bb4b4eca4af5248ef5398855
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2024-04-25 16:28:05 +0200

    clean up unneeded code

--- cos/packages/installer.lua
+++ cos/packages/installer.lua
@@ -1,8 +1,3 @@
-local args = {...}
-if args[1] == "install-cos" then
-  -- when installing cOS from a regular computer, we'll find ourselves here
-end
-
 local installer = {}
 installer.metadata = {
     name = "installer",
