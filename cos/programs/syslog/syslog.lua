a2d25cb040e6e09eb2cb1023b108510155192bf2
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2024-04-25 15:04:48 +0200

    fix checkalive function

--- cos/programs/syslog/syslog.lua
+++ cos/programs/syslog/syslog.lua
@@ -5,17 +5,21 @@ if not args[1] then
     print("clear - clears the log file")
 end
 
-local checkAlive = function()
+local function checkAlive()
+    local alive = false
     local function check()
-        os.queueEvent("syslog_daemon")
+        os.queueEvent("syslog_daemon","ping")
+        print("sent queue event", true)
         os.pullEvent("syslog_daemon_response")
-        return true
+        print("got response", true)
+        alive = true
     end
     local function timeout()
         sleep(1)
-        return false
+        log("syslog isn't active!!",true)
     end
     parallel.waitForAny(timeout,check)
+    return alive
 end
 
 if not checkAlive() then error("daemon isn't alive",0) end
